85c80353ebfad70d977f5ab6a0114ed4f6867d46
Arthur Santos <arthur.luiz@gmail.com>
2014-07-08 10:32:11 -0300

    Updating default attributes on rails-base cookbook to be applied by the rvm::user recipe

--- chef/cookbooks/rails-base/attributes/default.rb
+++ chef/cookbooks/rails-base/attributes/default.rb
@@ -1,7 +1,17 @@
 node.default['rvm']['default_ruby'] = 'ruby-2.1'
 node.default['rvm']['user_default_ruby'] = 'ruby-2.1'
 
-node.default['rvm']['global_gems'] = [
-  { 'name'    => 'bundler' },
-  { 'name'    => 'rails' }
+node.default['rvm']['user_installs'] = [
+  {
+    :user => 'vagrant',
+    :default_ruby => 'ruby-2.1',
+    :global_gems => [
+      {
+        :name => 'bundler'
+      },
+      {
+        :name => 'rails'
+      }
+    ]
+  }
 ]
